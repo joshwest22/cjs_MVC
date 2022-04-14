e725a7888b2e33c3b4b92cbdfafc0bca19401b49
Josh West <josh.west@centre.edu>
2022-04-14 15:17:00 -0400

    libraries and dependencies not recognized

---
+++ src/main/java/views/AdderViewTest.java
@@ -0,0 +1,21 @@
+package views;
+
+import org.testfx.assertions.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+@ExtendWith(ApplicationExtension.class)
+public class AdderViewTest
+{
+
+}
